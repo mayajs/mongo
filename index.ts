3181c73df9a37d79a06e7e5aed879ff5793fc9df
Mac Ignacio <mack_ignacio@yahoo.com>
2019-08-23 06:49:52 +0800

    Add models

--- index.ts
+++ index.ts
@@ -2,3 +2,5 @@ import { connect, connection } from "mongoose";
 import { Database, MongoConnectionOptions } from "@mayajs/core";
 import { IPaginateModel } from "./lib/Interfaces";
 import paginate from "mongoose-paginate";
+
+const models: IPaginateModel[] = [];
