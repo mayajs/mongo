342cc81fa2c716db6195df097b37987c9d9eeb76
Mac Ignacio <mack_ignacio@yahoo.com>
2019-08-23 06:50:26 +0800

    Add Models decorator function

--- index.ts
+++ index.ts
@@ -4,3 +4,31 @@ import { IPaginateModel } from "./lib/Interfaces";
 import paginate from "mongoose-paginate";
 
 const models: IPaginateModel[] = [];
+
+export function Models(name: string): (target: any, key: string) => void {
+  return (target: any, key: string): void => {
+    // property value
+    let value = target[key];
+
+    // property getter method
+    const getter = () => {
+      return models.filter(e => e[name])[0][name];
+    };
+
+    // property setter method
+    const setter = (newVal: string) => {
+      value = models.filter(e => e[newVal])[0][newVal];
+    };
+
+    // Delete property.
+    if (delete target[key]) {
+      // Create new property with getter and setter
+      Object.defineProperty(target, key, {
+        configurable: true,
+        enumerable: true,
+        get: getter,
+        set: setter,
+      });
+    }
+  };
+}
