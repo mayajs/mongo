00a2730ee925c0197f084e27a23f2f2618d1d71b
Mac Ignacio <mack_ignacio@yahoo.com>
2019-08-23 06:49:34 +0800

    Add index.ts

---
+++ index.ts
@@ -0,0 +1,4 @@
+import { connect, connection } from "mongoose";
+import { Database, MongoConnectionOptions } from "@mayajs/core";
+import { IPaginateModel } from "./lib/Interfaces";
+import paginate from "mongoose-paginate";
